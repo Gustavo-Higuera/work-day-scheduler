10da1b5953cf7c73c019ec587a4a754dc966e81f
Coding Padawan <gustavohiguera9715@gmail.com>
2022-07-23 12:48:47 -0700

    finished time validation for blocks

--- assets/script.js
+++ assets/script.js
@@ -4,29 +4,50 @@
 var dayEl = $("#currentDay");
 var hourEl = $(".hour");
 
-function getTime() {
-    $('#currentDay').text(moment().format("MMM Do, YYYY"));
-  
+function getDate() {
+  $(dayEl).text(moment().format("MMM Do, YYYY"));
+
+}
+
+function checkTimeBlock() {
+  // this is setting the current time to just the day and hour it currently is, this is setting the minute second and ms to 0
+  var currentTime = moment().set({
+    "minute": "0",
+    "second": "0",
+    "millisecond": "0"
+  });
+
+  console.log(currentTime);
+
+  // each row class in the html will be targeted by this each function
+  $(".row").each(function () {
+    // "this" is referring to the .row class, and it is targeting the child w/ class .hour, and pulling the text from it
+    var timeBlockText = $(this).children(".hour").text();
+    console.log(timeBlockText);
+
+    // the text that was saved into timeBlockText is being converted into a moment object and is given the moment format of hA
+    var timeBlock = moment(timeBlockText, "hA");
+    console.log(timeBlock);
+
+    // WHEN I scroll down
+    // THEN I am presented with time blocks for standard business hours
+    // WHEN I view the time blocks for that day
+    // THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+    if (timeBlock.isBefore(currentTime)) {
+      $(this).children(".timeblock").addClass("past");
+    } else if (timeBlock.isAfter(currentTime)) {
+      $(this).children(".timeblock").addClass("future");
+    } else {
+      $(this).children(".timeblock").addClass("present");
+    }
+  })
 }
 
 
 
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-function checkTimeBlock () {
-  var timeBlockEl = $('.timeblock');
-  var currentHour = moment().format("hA");
-  console.log(currentHour);
 
-  var timeBlockHour = $(hourEl).text();
-  console.log(timeBlockHour);
 
-  if (timeBlockHour.isBefore(currentHour)){
-    timeBlockEl.addClass('past');
-  }
-}
 // WHEN I click into a time block
 // THEN I can enter an event
 // WHEN I click the save button for that time block
@@ -35,5 +56,5 @@ function checkTimeBlock () {
 // THEN the saved events persist
 
 
-getTime();
+getDate();
 checkTimeBlock();
